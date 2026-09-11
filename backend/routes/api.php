91e567cba8887e0933ff076b7fab41ddfedf63b5
Neemagrg <neemagurung1013@gmail.com>
2026-09-11 12:40:24 +0545

    Success

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,46 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+/*
+|--------------------------------------------------------------------------
+| Public API Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::post(
+    '/register',
+    [AuthController::class, 'register']
+);
+
+Route::post(
+    '/login',
+    [AuthController::class, 'login']
+);
+
+
+/*
+|--------------------------------------------------------------------------
+| Protected API Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post(
+        '/logout',
+        [AuthController::class, 'logout']
+    );
+
+    Route::get(
+        '/me',
+        [AuthController::class, 'me']
+    );
+
+    Route::apiResource(
+        'tasks',
+        TaskController::class
+    );
+});
