6034ed549c7adebe2abab1ecaf1843981c6fbea3
Neemagrg <neemagurung1013@gmail.com>
2026-09-11 12:35:49 +0545

    Success

--- backend/app/Models/Task.php
+++ backend/app/Models/Task.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'category',
+        'priority',
+        'status',
+        'due_date',
+    ];
+
+    protected $casts = [
+        'due_date' => 'date:Y-m-d',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
